c64a26ead331f991b241fcb9de362103acd1cc5c
David Straub <straub@protonmail.com>
2021-08-03 10:11:22 +0200

    Add web API handler

--- webapisync.py
+++ webapisync.py
@@ -20,6 +20,13 @@
 
 """Gramps addon to synchronize with a Gramps Web API server."""
 
+import json
+from pathlib import Path
+from tempfile import NamedTemporaryFile
+from typing import Optional
+from urllib.error import HTTPError
+from urllib.request import Request, urlopen
+
 from gramps.gui.plug.tool import BatchTool, ToolOptions
 
 
@@ -31,5 +38,70 @@ class WebApiSyncTool(BatchTool):
         if self.fail:
             return
 
+
 class WebApiSyncOptions(ToolOptions):
     """Options for Web API Sync."""
+
+
+class WebApiHandler:
+    """Web API connection handler."""
+
+    def __init__(self, url: str, username: str, password: str) -> None:
+        """Initialize given URL, user name, and password."""
+        self.url = url.rstrip("/")
+        self.username = username
+        self.password = password
+        self._access_token: Optional[str] = None
+
+    @property
+    def access_token(self) -> str:
+        """Get the access token."""
+        if not self._access_token:
+            self.fetch_token()
+        return self._access_token
+
+    def fetch_token(self) -> str:
+        """Fetch an access token."""
+        data = json.dumps({"username": self.username, "password": self.password})
+        req = Request(
+            f"{self.url}/token/",
+            data=data.encode(),
+            headers={"Content-Type": "application/json"},
+        )
+        try:
+            res = urlopen(req)
+        except HTTPError:
+            raise
+        try:
+            res_json = json.load(res)
+        except (UnicodeDecodeError, json.JSONDecodeError):
+            raise
+        self._access_token = res_json["access_token"]
+
+    def download_xml(self, retry: bool = True) -> Path:
+        """Download an XML export and return the path of the temp file."""
+        req = Request(
+            f"{self.url}/exporters/gramps/file",
+            headers={"Authorization": f"Bearer {self.access_token}"},
+        )
+        try:
+            res = urlopen(req)
+            temp = NamedTemporaryFile(delete=False)
+            chunk_size = 1024
+            chunk = res.read(chunk_size)
+            temp.write(chunk)
+            while chunk:
+                chunk = res.read(chunk_size)
+                temp.write(chunk)
+        except HTTPError as exc:
+            print(exc.code)
+            if exc.code == 401 and retry:
+                # in case of 401, retry once with a new token
+                self.fetch_token()
+                return self.download_xml(retry=False)
+            raise
+        return Path(temp.name)
+
+    def apply_changes(self, trans):
+        """Apply the changes to the remote database."""
+        raise NotImplementedError
