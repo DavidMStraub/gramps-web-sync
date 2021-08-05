2d4d82c1833ac126861dd60d9bcd4a19ef478682
David Straub <straub@protonmail.com>
2021-08-05 22:58:20 +0200

    Add details dialog, config file, commit local

--- webapisync.py
+++ webapisync.py
@@ -26,16 +26,22 @@ import os
 from pathlib import Path
 from tempfile import NamedTemporaryFile
 from time import sleep
-from typing import Callable, List, Optional, Set, Tuple
+from typing import Any, Callable, Dict, List, Optional, Set, Tuple
 from urllib.error import HTTPError, URLError
 from urllib.parse import urlparse
 from urllib.request import Request, urlopen
 
+import gramps
 from gi.repository import Gtk
+from gramps.gen.config import config as configman
 from gramps.gen.const import GRAMPS_LOCALE as glocale
+from gramps.gen.db import KEY_TO_CLASS_MAP, DbTxn
 from gramps.gen.db.base import DbReadBase
+from gramps.gen.db.dbconst import TXNADD, TXNDEL, TXNUPD
+from gramps.gen.db.txn import DbTxn
 from gramps.gen.db.utils import import_as_dict
 from gramps.gen.lib.primaryobj import BasicPrimaryObject as GrampsObject
+from gramps.gen.lib.serialize import to_json
 from gramps.gen.merge.diff import diff_dbs
 from gramps.gen.user import User
 from gramps.gui.dialog import ErrorDialog, QuestionDialog2, WarningDialog
@@ -65,17 +71,16 @@ OBJ_LST = [
 ]
 
 # actions: add, delete, update, merge - local/remote
-A_ADD_LOC = 0
-A_ADD_REM = 1
-A_DEL_LOC = 2
-A_DEL_REM = 3
-A_UPD_LOC = 4
-A_UPD_REM = 5
-A_MRG_LOC = 6
-A_MRG_REM = 7
-A_CONFLICT = 8
+A_ADD_LOC = "add_local"
+A_ADD_REM = "add_remote"
+A_DEL_LOC = "del_local"
+A_DEL_REM = "del_remote"
+A_UPD_LOC = "upd_local"
+A_UPD_REM = "upd_remote"
+A_MRG_REM = "mrg_remote"
 
-Actions = List[Tuple[int, str, str, GrampsObject, Optional[GrampsObject]]]
+Action = Tuple[int, str, str, Optional[GrampsObject], Optional[GrampsObject]]
+Actions = List[Action]
 
 
 class WebApiSyncTool(BatchTool):
@@ -83,6 +88,11 @@ class WebApiSyncTool(BatchTool):
 
     def __init__(self, dbstate, user, options_class, name, *args, **kwargs) -> None:
         super().__init__(dbstate, user, options_class, name)
+        self.config = configman.register_manager("webapisync")
+        self.config.register("credentials.url", "")
+        self.config.register("credentials.username", "")
+        self.config.register("credentials.password", "")
+        self.config.load()
         if self.fail:
             return
         db1 = dbstate.db
@@ -90,17 +100,25 @@ class WebApiSyncTool(BatchTool):
         if db2 is None:
             return
         self.sync = WebApiSyncDiffHandler(db1, db2, user=self._user)
-        actions = self.sync.get_actions()
-        actions = self.edit_actions(actions)
-        print(actions)
+        self.actions = self.sync.get_actions()
+        self.diff_dialog()
 
     def get_remote_db(self) -> Optional[DbReadBase]:
         """Download the remote data and return it as in-memory database."""
-        login = LoginDialog(self._user.uistate)
+        login = LoginDialog(
+            self._user.uistate,
+            url=self.config.get("credentials.url"),
+            username=self.config.get("credentials.username"),
+            password=self.config.get("credentials.password"),
+        )
         credentials = login.run()
         if credentials is None:
             return None
         url, username, password = credentials
+        self.config.set("credentials.url", url)
+        self.config.set("credentials.username", username)
+        self.config.set("credentials.password", password)
+        self.config.save()
         self.api = WebApiHandler(url, username, password)
         path = self.handle_server_errors(self.api.download_xml)
         if path is None:
@@ -138,19 +156,29 @@ class WebApiSyncTool(BatchTool):
             ErrorDialog(_("Error while parsing response from server."))
             return None
 
-    def edit_actions(self, actions: Actions) -> Actions:
+    def diff_dialog(self) -> bool:
         """Edit the automatically generated actions via user interaction."""
-        return actions  # FIXME
+        dialog = DiffDetailDialog(self._user.uistate, self.actions, on_ok=self.commit)
+        dialog.show()
+
+    def commit(self):
+        """Commit all changes to the databases."""
+        msg = "Apply Web API Sync changes"
+        with DbTxn(msg, self.sync.db1) as trans1:
+            with DbTxn(msg, self.sync.db2) as trans2:
+                self.sync.commit_actions(self.actions, trans1, trans2)
+                self.api.commit(trans2)
 
 
 class LoginDialog(ManagedWindow):
     """Login dialog."""
 
-    def __init__(self, uistate):
+    def __init__(self, uistate, url=None, username=None, password=None):
         """Initialize self."""
         self.title = _("Login")
         super().__init__(uistate, [], self.__class__, modal=True)
         dialog = Gtk.Dialog(transient_for=uistate.window)
+        dialog.set_default_size(500, -1)
         grid = Gtk.Grid()
         grid.set_border_width(6)
         grid.set_row_spacing(6)
@@ -158,17 +186,23 @@ class LoginDialog(ManagedWindow):
         label = Gtk.Label(label=_("Server URL: "))
         grid.attach(label, 0, 0, 1, 1)
         self.url = Gtk.Entry()
+        if url:
+            self.url.set_text(url)
         self.url.set_hexpand(True)
         self.url.set_input_purpose(Gtk.InputPurpose.URL)
         grid.attach(self.url, 1, 0, 1, 1)
         label = Gtk.Label(label=_("Username: "))
         grid.attach(label, 0, 1, 1, 1)
         self.username = Gtk.Entry()
+        if username:
+            self.username.set_text(username)
         self.username.set_hexpand(True)
         grid.attach(self.username, 1, 1, 1, 1)
         label = Gtk.Label(label=_("Password: "))
         grid.attach(label, 0, 2, 1, 1)
         self.password = Gtk.Entry()
+        if password:
+            self.password.set_text(password)
         self.password.set_hexpand(True)
         self.password.set_visibility(False)
         self.password.set_input_purpose(Gtk.InputPurpose.PASSWORD)
@@ -217,6 +251,102 @@ class LoginDialog(ManagedWindow):
         return url
 
 
+class DiffDetailDialog(ManagedWindow):
+    """Dialog showing the differences before applying them."""
+
+    def __init__(self, uistate, actions: Actions, on_ok: Callable):
+        self.title = _("Differences")
+        self.on_ok = on_ok
+        super().__init__(uistate, [], self.__class__, modal=True)
+        # window
+        window = Gtk.Window()
+        window.set_default_size(600, 400)
+        window.set_border_width(6)
+
+        # tree view
+        self.store = self.actions_to_store(actions)
+        view = Gtk.TreeView(model=self.store)
+        # expand first level
+        for i, row in enumerate(self.store):
+            view.expand_row(Gtk.TreePath(i), False)
+
+        for i, col in enumerate(["ID", "Content"]):
+            renderer = Gtk.CellRendererText()
+            column = Gtk.TreeViewColumn(col, renderer, text=i)
+            view.append_column(column)
+
+        # scrolled window
+        scrolled_window = Gtk.ScrolledWindow()
+        scrolled_window.add(view)
+
+        # buttons
+        cancel_btn = Gtk.Button(_("Cancel"))
+        ok_btn = Gtk.Button(_("Apply"))
+        button_box = Gtk.ButtonBox(Gtk.Orientation.HORIZONTAL)
+        button_box.pack_start(cancel_btn, False, False, 0)
+        button_box.pack_start(ok_btn, False, False, 0)
+
+        # button callbacks
+        cancel_btn.connect("clicked", self._on_cancel)
+        ok_btn.connect("clicked", self._on_ok)
+
+        # grid
+        box = Gtk.Box.new(Gtk.Orientation.VERTICAL, 5)
+        box.set_margin_top(5)
+        box.set_margin_start(5)
+        box.set_margin_end(5)
+        box.set_margin_bottom(5)
+        box.pack_start(scrolled_window, True, True, 0)
+        box.pack_start(button_box, False, False, 0)
+
+        window.add(box)
+        self.set_window(window, None, self.title)
+
+    def _on_cancel(self, widget):
+        self.close()
+
+    def _on_ok(self, widget):
+        self.close()
+        self.on_ok()
+
+    def actions_to_store(self, actions: Actions) -> Gtk.TreeStore:
+        """Convert the actions list to a tree store."""
+        store = Gtk.TreeStore(str, str)
+        action_labels = {
+            _("Local changes"): {
+                _("Added"): A_ADD_REM,
+                _("Deleted"): A_DEL_REM,
+                _("Modified"): A_UPD_REM,
+            },
+            _("Remote changes"): {
+                _("Added"): A_ADD_LOC,
+                _("Deleted"): A_DEL_LOC,
+                _("Modified"): A_UPD_LOC,
+            },
+            _("Simultaneous changes"): {_("Modified"): A_MRG_REM},
+        }
+
+        for label1, v1 in action_labels.items():
+            iter1 = store.append(None, [label1, ""])
+            for label2, action_type in v1.items():
+                rows = []
+                for action in actions:
+                    _type, handle, class_name, obj1, obj2 = action
+                    if _type == action_type:
+                        if obj1 is not None:
+                            gid = obj1.gramps_id
+                        else:
+                            gid = obj2.gramps_id
+                        obj_details = [class_name, gid]
+                        rows.append(obj_details)
+                if rows:
+                    label2 = f"{label2} ({len(rows)})"
+                    iter2 = store.append(iter1, [label2, ""])
+                    for row in rows:
+                        store.append(iter2, row)
+        return store
+
+
 class WebApiSyncOptions(ToolOptions):
     """Options for Web API Sync."""
 
@@ -250,7 +380,8 @@ class WebApiHandler:
         try:
             res_json = json.load(res)
         except (UnicodeDecodeError, json.JSONDecodeError):
-            raise ValueError("Error while parsing response.")
+            self.url = f"{self.url}/api"
+            return self.fetch_token()
         self._access_token = res_json["access_token"]
 
     def download_xml(self, retry: bool = True) -> Path:
@@ -283,9 +414,10 @@ class WebApiHandler:
         os.remove(temp.name)
         return Path(unzipped_name)
 
-    def apply_changes(self, trans):
-        """Apply the changes to the remote database."""
-        raise NotImplementedError
+    def commit(self, trans):
+        """Commit the changes to the remote database."""
+        payload = transaction_to_json(trans)
+        print(payload)  # FIXME
 
 
 class WebApiSyncDiffHandler:
@@ -443,13 +575,13 @@ class WebApiSyncDiffHandler:
         """Get a list of objects and corresponding actions."""
         lst = []
         for (obj1, obj2) in self.modified_in_both:
-            lst.append((A_CONFLICT, obj1.handle, obj1.__class__.__name__, obj1, obj2))
+            lst.append((A_MRG_REM, obj1.handle, obj1.__class__.__name__, obj1, obj2))
         for obj in self.added_to_db1:
             lst.append((A_ADD_REM, obj.handle, obj.__class__.__name__, obj, None))
         for obj in self.added_to_db2:
-            lst.append((A_ADD_LOC, obj.handle, obj.__class__.__name__, obj, None))
+            lst.append((A_ADD_LOC, obj.handle, obj.__class__.__name__, None, obj))
         for obj in self.deleted_from_db1:
-            lst.append((A_DEL_REM, obj.handle, obj.__class__.__name__, obj, None))
+            lst.append((A_DEL_REM, obj.handle, obj.__class__.__name__, None, obj))
         for obj in self.deleted_from_db2:
             lst.append((A_DEL_LOC, obj.handle, obj.__class__.__name__, obj, None))
         for (obj1, obj2) in self.modified_in_db1:
@@ -457,3 +589,50 @@ class WebApiSyncDiffHandler:
         for (obj1, obj2) in self.modified_in_db2:
             lst.append((A_UPD_LOC, obj1.handle, obj1.__class__.__name__, obj1, obj2))
         return lst
+
+    def commit_action(self, action: Action, trans1: DbTxn, trans2: DbTxn) -> None:
+        """Commit an action into local and remote transaction objects."""
+        typ, handle, obj_type, obj1, obj2 = action
+        if typ == A_DEL_LOC:
+            self.db1.method("remove_%s", obj_type)(handle, trans1)
+        elif typ == A_DEL_REM:
+            self.db2.method("remove_%s", obj_type)(handle, trans2)
+        elif typ == A_ADD_LOC:
+            self.db1.method("add_%s", obj_type)(obj2, trans1)
+        elif typ == A_ADD_REM:
+            self.db2.method("add_%s", obj_type)(obj1, trans2)
+        elif typ == A_UPD_LOC:
+            self.db1.method("commit_%s", obj_type)(obj2, trans1)
+        elif typ == A_UPD_REM:
+            self.db2.method("commit_%s", obj_type)(obj1, trans2)
+
+    def commit_actions(self, actions: Actions, trans1: DbTxn, trans2: DbTxn) -> None:
+        """Commit several actions into local and remote transaction objects."""
+        for action in actions:
+            self.commit_action(action, trans1, trans2)
+
+
+def transaction_to_json(transaction: DbTxn) -> List[Dict[str, Any]]:
+    """Return a JSON representation of a database transaction."""
+    out = []
+    for recno in transaction.get_recnos(reverse=True):
+        key, action, handle, old_data, new_data = transaction.get_record(recno)
+        try:
+            obj_cls_name = KEY_TO_CLASS_MAP[key]
+        except KeyError:
+            continue  # this happens for references
+        trans_dict = {TXNUPD: "update", TXNDEL: "delete", TXNADD: "add"}
+        obj_cls = getattr(gramps.gen.lib, obj_cls_name)
+        if old_data:
+            old_data = obj_cls().unserialize(old_data)
+        if new_data:
+            new_data = obj_cls().unserialize(new_data)
+        item = {
+            "type": trans_dict[action],
+            "handle": handle,
+            "_class": obj_cls_name,
+            "old": json.loads(to_json(old_data)),
+            "new": json.loads(to_json(new_data)),
+        }
+        out.append(item)
+    return out
